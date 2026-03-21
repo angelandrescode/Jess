25197288425f748de6ffb2558b082d392d48e6f4
Angel Andres <andressamaniegosandoval@gmail.com>
2026-03-21 19:47:05 +0100

    Speech-text better detection of voice

--- src/speech-text.ts
+++ src/speech-text.ts
@@ -9,12 +9,13 @@ import VAD from "node-vad";
 import { StateManager } from "./global-states";
 
 let counterChunksSilence = 0;
+let counterChunksVoice = 0;
 let thresholdCounterChunksSilence = 10;
+let thresholdCounterChunkVoice = 3; // Lo suficiente como para no perder tanta información.
 let newSpeechWasCommited = false;
 const bytesEachChunk = 3200; // para 100ms de audio a 16kHz y 16 bits (2 bytes por muestra) mono: 16000 muestras/seg * 0.1 seg * 2 bytes/muestra = 3200 bytes
 let totalBufferToSend = Buffer.alloc(0); // Buffer que va a guardar un chunk de audio que se mandara a eleven labs
 const vad = new VAD(VAD.Mode.AGGRESSIVE);
-
 async function commitSystemOnChunk(
   connectionToElevenLabs: RealtimeConnection,
   chunk: Buffer, //3200bytes,
@@ -24,6 +25,8 @@ async function commitSystemOnChunk(
     console.log("silencio");
     ++counterChunksSilence;
     if (counterChunksSilence < thresholdCounterChunksSilence) return;
+    // Devolvemos el contador de chunks de voz para que vuelva a contar desde 0 para el proximo speech.
+    counterChunksVoice = 0;
     //Si el commit no se hizo antes, hacerlo, y bloquear el hecho de que se pueda hacer luego, para evitar el error de que el buffer no puede ser menor a 0.3s.
     //Ademas evita commitear buffers vacios
     if (!newSpeechWasCommited) {
@@ -39,10 +42,15 @@ async function commitSystemOnChunk(
   }
   if (speechEvent === VAD.Event.VOICE) {
     console.log("Se detecto nuevo speech, el commit se desbloquea");
+    ++counterChunksVoice;
+    // para que no detecte como "voz" al minimo susurro
+    if (counterChunksVoice < thresholdCounterChunkVoice) return;
     // Se resetea el counter de chunks silence para que el contador vuelva a empezar desde el siguiente silencio
     counterChunksSilence = 0;
     // Cuando empieza a hablar, empieza otro "speech" otra cosa que hay que COMMITEAR, por eso, se desbloquea el commiteo con la flag para que sea capaz de commitear.
     newSpeechWasCommited = false;
+    StateManager.setIsTurnOfUser(true);
+    StateManager.setIsTurnOfJess(false);
   }
 }
 
