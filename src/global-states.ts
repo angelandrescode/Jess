a6247a0a6588fc4bcea0e9dd7f8be07f7453952f
Angel Andres <andressamaniegosandoval@gmail.com>
2026-03-21 19:47:56 +0100

    Reactive states added

--- src/global-states.ts
+++ src/global-states.ts
@@ -1,15 +1,32 @@
+type StateValue<T> = {
+  value: T;
+  callbacksOnChange: Array<(value: T) => void>;
+};
+
 type StateType = {
-  isListening: boolean;
-  isInConversation: boolean;
-  isTurnOfJess: boolean;
-  isTurnOfUser: boolean;
+  isListening: StateValue<boolean>;
+  isInConversation: StateValue<boolean>;
+  isTurnOfJess: StateValue<boolean>;
+  isTurnOfUser: StateValue<boolean>;
 };
 
 let states: StateType = {
-  isListening: false,
-  isInConversation: false,
-  isTurnOfJess: false,
-  isTurnOfUser: false,
+  isListening: {
+    value: false,
+    callbacksOnChange: [],
+  },
+  isInConversation: {
+    value: false,
+    callbacksOnChange: [],
+  },
+  isTurnOfJess: {
+    value: false,
+    callbacksOnChange: [],
+  },
+  isTurnOfUser: {
+    value: false,
+    callbacksOnChange: [],
+  },
 };
 
 export const StateManager = {
@@ -17,15 +34,39 @@ export const StateManager = {
     return states;
   },
   setIsListening(value: boolean) {
-    states.isListening = value;
+    states.isListening.value = value;
+    states.isListening.callbacksOnChange.forEach((callback) =>
+      callback(states.isListening.value),
+    );
+  },
+  subscribeToIsListening(callback: (value: boolean) => void) {
+    states.isListening.callbacksOnChange.push(callback);
   },
   setIsInConversation(value: boolean) {
-    states.isInConversation = value;
+    states.isInConversation.value = value;
+    states.isInConversation.callbacksOnChange.forEach((callback) =>
+      callback(states.isInConversation.value),
+    );
+  },
+  subscribeToIsInConversation(callback: (value: boolean) => void) {
+    states.isInConversation.callbacksOnChange.push(callback);
   },
   setIsTurnOfJess(value: boolean) {
-    states.isTurnOfJess = value;
+    states.isTurnOfJess.value = value;
+    states.isTurnOfJess.callbacksOnChange.forEach((callback) =>
+      callback(states.isTurnOfJess.value),
+    );
+  },
+  subscribeToIsTurnOfJess(callback: (value: boolean) => void) {
+    states.isTurnOfJess.callbacksOnChange.push(callback);
   },
   setIsTurnOfUser(value: boolean) {
-    states.isTurnOfUser = value;
+    states.isTurnOfUser.value = value;
+    states.isTurnOfUser.callbacksOnChange.forEach((callback) =>
+      callback(states.isTurnOfUser.value),
+    );
+  },
+  subscribeToIsTurnOfUser(callback: (value: boolean) => void) {
+    states.isTurnOfUser.callbacksOnChange.push(callback);
   },
 };
